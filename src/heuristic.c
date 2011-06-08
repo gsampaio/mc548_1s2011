f95d51dd47eb9fdd5e86e64deebe1080265c1014
Fabiano Fidêncio <fabianofidencio@gmail.com>
2011-06-08 01:53:09 -0300

    (Codying style)++

--- src/heuristic.c
+++ src/heuristic.c
@@ -2,13 +2,12 @@
 #include <Eina.h>
 #include "save_word.h"
 
-Solution* compare_solution(Solution*s1, Solution*s2) {
-    if (s1->value == s2->value)
+Solution *
+compare_solution(Solution *s1, Solution *s2)
+{
+    if (s1->value >= s2->value)
         return s1;
-    else if (s1->value > s2->value)
-        return s1;
-    else
-        return s2;
+    return s2;
 }
 
 
