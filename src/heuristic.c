6460afbbb6b04e74d34663073398592eaeb89254
Fabiano Fidêncio <fabianofidencio@gmail.com>
2011-06-08 03:26:56 -0300

    Adicionando solution_update
        - Essa função recebe uma Station s e a partir disso faz:
            - Adiciona s na lista de Stations da solução
            - Remove s da lista de Stations disponiveis
            - Remove os pontos cobertos por s da lista de pontos não
            cobertos

--- src/heuristic.c
+++ src/heuristic.c
@@ -8,6 +8,33 @@ solution_compare(Solution *s1, Solution *s2)
     return s2;
 }
 
+void
+solution_update(Solution *solution, Eina_List *points, Eina_List *stations, Station *station)
+{
+    Eina_List *l1;
+    int *d1;
+
+    EINA_LIST_FOREACH(station->points, l1, d1)
+    {
+        Eina_List *l2, *l2_next;
+        int *d2;
+        EINA_LIST_FOREACH_SAFE(points, l2, l2_next, d2)
+        {
+            if (*d1 == *d2)
+            {
+                /*FIXME: retirar esse check depois de testar*/
+                if (d1 == d2)
+                    printf("Fuuuudeu\n");
+                points = eina_list_remove(points, d2);
+                free(d2);
+            }
+        }
+    }
+
+    stations = eina_list_remove(stations, station);
+    solution = eina_list_append(station);
+}
+
 Station *
 station_random_get(Eina_List *stations)
 {
