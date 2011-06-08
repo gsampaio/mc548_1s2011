212389d615b510cc641cd018851a0e7eb4f3f510
Fabiano Fidêncio <fabianofidencio@gmail.com>
2011-06-08 00:04:33 -0300

    Codying style)++

--- include/save_world.h
+++ include/save_world.h
@@ -20,10 +20,10 @@ struct _Station
     Eina_List *points;
 };
 
-typedef struct _Solution Solution; 
+typedef struct _Solution Solution;
 struct _Solution {
-	double value; 
-	Eina_List * stations;
-}
+    double value;
+    Eina_List *stations;
+};
 
 #endif /* SAVE_WORLD_H__ */
