3a2da9b688d22eca30a478ecf7d7e8cbbcd074f1
Fabiano Fidêncio <fabianofidencio@gmail.com>
2011-06-08 02:17:35 -0300

    Padronizando nomes

--- src/heuristic.c
+++ src/heuristic.c
@@ -1,7 +1,7 @@
 #include "save_word.h"
 
 Solution *
-compare_solution(Solution *s1, Solution *s2)
+solution_compare(Solution *s1, Solution *s2)
 {
     if (s1->value >= s2->value)
         return s1;
